058218740375a3f12e215e379c42c1ebd6b4fdeb
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-06 12:37:34 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается.
+
+bool circle = true;
+string stopCircle = "exit";
+
+while(circle)
+{
+    string userAnswer = ReadIn("Guess the word: ");
+
+    int result = string.Compare(userAnswer.ToLower(),stopCircle);
+    if (result==0)
+    {
+        Console.WriteLine("Excellent!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong word. Try again.");
+    }
+}
+
+string ReadIn(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToString(Console.ReadLine())??String.Empty;
+}
