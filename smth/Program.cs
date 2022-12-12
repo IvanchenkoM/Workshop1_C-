aad15b5ba1ed8718a1bccf4e87bd0147a4ceceef
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-12 22:14:43 +0300

    add smth

---
+++ smth/Program.cs
@@ -0,0 +1,10 @@
+﻿int rows = 6;
+int startingPoint = 7;
+int i=1;
+double smthCalculation = Math.Round(Convert.ToDouble((1-(rows*startingPoint))/(1-startingPoint)),2);
+
+Console.WriteLine(smthCalculation);
+double exactCalculation = Math.Round(Convert.ToDouble((i-smthCalculation)/(rows-smthCalculation)),2);
+Console.WriteLine(exactCalculation);
+
+int x = Convert.ToInt32(exactCalculation);
